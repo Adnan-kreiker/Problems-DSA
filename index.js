ee9c719959e4100ff1a02c8bb66be637d7a23769
Adnan Kreiker <81513811+Adnan-kreiker@users.noreply.github.com>
2022-06-03 19:40:16 +0300

    Add collectStrings

--- index.js
+++ index.js
@@ -2040,3 +2040,36 @@ function stringifyNumbers(nestedObj){
 }
 
 console.log(stringifyNumbers(obj))
+
+//===================================================================================================
+// collectStrings
+// Write a function called collectStrings which accepts an object and
+// returns an array of all the values in the object that have a typeof string.
+const obj1 = {
+  stuff: 'foo',
+  data: {
+    val: {
+      thing: {
+        info: 'bar',
+        moreInfo: {
+          evenMoreInfo: {
+            weMadeIt: 'baz'
+          }
+        }
+      }
+    }
+  }
+};
+function collectStrings(obj){
+  let res = []
+  for(let key in obj) {
+    if(typeof obj[key] === 'string') {
+      res.push(obj[key])
+    } else if(typeof obj[key] === 'object') {
+     res = res.concat(collectStrings(obj[key]))
+    }
+  }
+  return res
+}
+
+console.log(collectStrings(obj1))
